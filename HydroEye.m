7f90f58511c2139390f39a3a327dbe27292a338f
Feras <feras@std.sa>
2025-05-09 11:23:03 +0300

    Updated parameters, added new servo & thurst data

--- HydroEye.m
+++ HydroEye.m
@@ -1,50 +1,39 @@
 %% HITL Params
-SampleTime = 0.01;
+SampleTime = 1/400;
 MAVLink_Input_Read_Size = 1024;
-
+%% Damping Coeff
+Clp = -0.4996148;
+Cnp = -0.0556744;
+Clr = 0.1223343;
+Cnr = -0.0422534;
+Cmq = -0.0542848;
+%% Servo PWM
+MG995_PWM = 500:11:2500; % np = 1500
+SPT5410LV_PWM = 500:11:2500; % np = 1500
+Servo_Angle = -180:2:180;
 %% Simulink Params
 % Physical Properties
-mass = 7;
-I = [0.32 -0.01 0.13;
-    -0.01  2.65   0;
-     0.13   0   2.92; ];
+mass = 8.2;
+I = [0.74   0     0;
+    -0.01  3.39   0;
+     0.13   0   4.07];
 
-cg = [0.5808 0 0.01];
+cg = [0.5701 0 0.01];
 cp = [0.519 0 -0018];
 
-Sref = 0.57; %m2
-b = 2; %m
-c = 0.35; %m
+Sref = 0.62; %m2
+b = 2.25; %m
+c = 0.30; %m
 
 % 6 DOF Block Initial conditions
-init.v = [19 0 0];
+init.v = [0 0 0];
 init.angles = [0 0 0];
 init.angRates = [0 0 0];
-init.pos = [0 0 -120]; % meters
+init.pos = [0 0 0]; % meters
 
+throttle = [0 40 45 50 55 60 65 70 75 80 90 100];
+thrust  = [0 1798 2069 2380 2771 3241 3738 4145 4635 5207 6054 6618]/1000;
+PWM = [1000 1400 1450 1500 1550 1600 1650 1700 1750 1800 1900 2000];
 
-thrust  = [1798 2069 2380 2771 3241 3738 4145 4635]/1000;
 power = [253.85 307.76 371.33 446.90 568.11 693.11 810.01 955.01];
 current = [11.43 13.89 16.79 20.26 	25.87 31.71 37.21 44.10	];
-
-% Coefficients from a .csv method
-% longitudinal
-elevator = unique(AeroLong.elevator);
-alpha = unique(AeroLong.alpha);
-CL = reshape(AeroLong.CL,length(alpha),length(elevator))
-CD = reshape(AeroLong.CD,length(alpha),length(elevator))
-Cm = reshape(AeroLong.Cm,length(alpha),length(elevator))
-Cmq = -5.4623*pi/180
-
-% lateral
-rudder = unique(AeroLat.rudder);
-beta = unique(AeroLat.beta);
-CY = reshape(AeroLat.CY,length(beta),length(rudder))
-Cl = reshape(AeroLat.Cl,length(beta),length(rudder))
-Cn = reshape(AeroLat.Cn,length(beta),length(rudder))
-Clp = -0.46415*pi/180
-Cnp = -0.05958*pi/180
-Cnr = -0.03820*pi/180
-Clr = 0.12437*pi/180
-
-save('aerocoef.mat',"alpha","Cm","CD","CL","elevator",'cg','beta','rudder','CY','Cl','Cn','Cmq','Cnr','Clp','Cnp');
